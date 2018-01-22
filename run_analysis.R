a19da214284cec70b1682302ba91fbbfa5418958
rebecccachan <33663204+rebecccachan@users.noreply.github.com>
2018-01-22 13:23:47 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -13,55 +13,76 @@
 ##########################################################################################################
 
 
-# Step 1
+# Step1. Merges the training and the test sets to create one data set.
 # to set my working directory
 setwd("C:/Users/t23bybh/Documents/UCI HAR Dataset")
 
-# load features, extract them and subset only the indeces of means and st devs
-featureNames <- read.table("features.txt")
-ourFeatures <- grep("std|mean", featureNames$V2)
+trainData <- read.table("./train/X_train.txt")
+dim(trainData) # 7352*561
+head(trainData)
+trainLabel <- read.table("./train/y_train.txt")
+table(trainLabel)
+trainSubject <- read.table("./train/subject_train.txt")
+testData <- read.table("./test/X_test.txt")
+dim(testData) # 2947*561
+testLabel <- read.table("./test/y_test.txt") 
+table(testLabel) 
+testSubject <- read.table("./test/subject_test.txt")
+joinData <- rbind(trainData, testData)
+dim(joinData) # 10299*561
+joinLabel <- rbind(trainLabel, testLabel)
+dim(joinLabel) # 10299*1
+joinSubject <- rbind(trainSubject, testSubject)
+dim(joinSubject) # 10299*1
 
-# Step 2
-# load train and test features, extract them and subset only means and st devs
-trainFeatures <- read.table("train/X_train.txt")
-ourTrainFeatures <- trainFeatures[,ourFeatures]
-testFeatures <- read.table("test/X_test.txt")
-ourTestFeatures <- test.features[,ourFeatures]
+# Step2. Extracts only the measurements on the mean and standard 
+# deviation for each measurement. 
+features <- read.table("./features.txt")
+dim(features)  # 561*2
+meanStdIndices <- grep("mean\\(\\)|std\\(\\)", features[, 2])
+length(meanStdIndices) # 66
+joinData <- joinData[, meanStdIndices]
+dim(joinData) # 10299*66
+names(joinData) <- gsub("\\(\\)", "", features[meanStdIndices, 2]) # remove "()"
+names(joinData) <- gsub("mean", "Mean", names(joinData)) # capitalize M
+names(joinData) <- gsub("std", "Std", names(joinData)) # capitalize S
+names(joinData) <- gsub("-", "", names(joinData)) # remove "-" in column names 
 
-# Step 3
-# merge the train and test data sets
-totalFeatures <- rbind(ourTrainFeatures, ourTestFeatures)
-# add column names to the features
-colnames(totalFeatures) <- featureNames[ourFeatures, 2]
+# Step3. Uses descriptive activity names to name the activities in 
+# the data set
+activity <- read.table("./activity_labels.txt")
+activity[, 2] <- tolower(gsub("_", "", activity[, 2]))
+substr(activity[2, 2], 8, 8) <- toupper(substr(activity[2, 2], 8, 8))
+substr(activity[3, 2], 8, 8) <- toupper(substr(activity[3, 2], 8, 8))
+activityLabel <- activity[joinLabel[, 1], 2]
+joinLabel[, 1] <- activityLabel
+names(joinLabel) <- "activity"
 
-# Step 4
-# Read and merge the train and test activity 
-trainActivities <- read.table("train/y_train.txt")
-testActivities <- read.table("test/y_test.txt")
-totalActivities <- rbind(trainActivities, testActivities)
+# Step4. Appropriately labels the data set with descriptive activity 
+# names. 
+names(joinSubject) <- "subject"
+cleanedData <- cbind(joinSubject, joinLabel, joinData)
+dim(cleanedData) # 10299*68
+write.table(cleanedData, "merged_data.txt") # write out the 1st dataset
 
-# extract activity labels and link to activity codes
-activityLabels <- read.table("activity_labels.txt")
-totalActivities$activity <- factor(totalActivities$V1, levels = activityLabels$V1, labels = activityLabels$V2)
-
-# Step 5
-# extract the train and test subject data and merge
-trainSubjects <- read.table("train/subject_train.txt")
-testSubjects <- read.table("test/subject_test.txt")
-totalSubjects <- rbind(trainSubjects, testSubjects)
-
-# Step 6
-# link the subjects to their activity
-subjectsActivities <- cbind(totalSubjects, totalActivities$activity)
-colnames(subjectsActivities) <- c("subjectID", "activity")
-
-# Step 7
-# Combine the activity, ID and measurement in a data frame
-activityDF <- cbind(subjectsActivities, totalFeatures)
-
-# Step 8
-# create an independent tidy data set with the average of each variable 
-# for each activity and each subject
-result <- aggregate(activityDF[,3:(ncol(activityDF))], by = list(activityDF$subjectID, activityDF$activity), FUN = mean)
-colnames(result)[1:2] <- c("subject.id", "activity")
-write.table(result, file="data_tidy.txt", row.names = FALSE)
+# Step5. Creates a second, independent tidy data set with the average of 
+# each variable for each activity and each subject. 
+subjectLen <- length(table(joinSubject)) # 30
+activityLen <- dim(activity)[1] # 6
+columnLen <- dim(cleanedData)[2]
+result <- matrix(NA, nrow=subjectLen*activityLen, ncol=columnLen) 
+result <- as.data.frame(result)
+colnames(result) <- colnames(cleanedData)
+row <- 1
+for(i in 1:subjectLen) {
+  for(j in 1:activityLen) {
+    result[row, 1] <- sort(unique(joinSubject)[, 1])[i]
+    result[row, 2] <- activity[j, 2]
+    bool1 <- i == cleanedData$subject
+    bool2 <- activity[j, 2] == cleanedData$activity
+    result[row, 3:columnLen] <- colMeans(cleanedData[bool1&bool2, 3:columnLen])
+    row <- row + 1
+  }
+}
+head(result)
+write.table(result, "data_with_means.txt") # write out the 2nd dataset
